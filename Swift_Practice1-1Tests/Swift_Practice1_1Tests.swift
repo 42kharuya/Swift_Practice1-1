075d3922bd97cfb7967fbe3bb556324d33e892c1
42kharuya <haruya.0411.k@gmail.com>
2025-01-28 14:35:14 +0900

    Initial Commit

---
+++ Swift_Practice1-1Tests/Swift_Practice1_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice1_1Tests.swift
+//  Swift_Practice1-1Tests
+//
+//  Created by 川﨑遥也 on 2025/01/28.
+//
+
+import Testing
+@testable import Swift_Practice1_1
+
+struct Swift_Practice1_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
